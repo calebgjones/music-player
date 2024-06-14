000586a74cf4514b946fce852eaee62cb6186aef
lichfiet <sortaminty@gmail.com>
2024-06-14 16:29:45 -0600

    added deleting songs from queue, and moved song logic to the main music player component

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -1,29 +1,43 @@
-import React from 'react';
+import { useState} from 'react';
 import MediaInterface from './musicPlayerComponents/MediaInterface/MediaInterface.jsx';
 import SongQueue from './musicPlayerComponents/SongQueue/SongQueue.jsx';
-import { songs } from './musicPlayerComponents/SongQueue/SongQueue.jsx';
 import AlbumArt from './musicPlayerComponents/AlbumArt/AlbumArt.jsx';
 
-export var currentSong = songs[0];
-
 function MusicPlayer() {
+  const songs = [
+    'Something - Remastered 2009 - The Beatles',
+    'Hey Jude - Remastered 2015 - The Beatles',
+    'Let It Be - Remastered 2009 - The Beatles',
+    'Help! - Remastered 2009 - The Beatles',
+    'Yesterday - Remastered 2009 - The Beatles',
+    'Come Together - Remastered 2009 - The Beatles',
+    'Twist And Shout - Remastered 2009 - The Beatles',
+    'I Want To Hold Your Hand - Remastered 2015 - The Beatles',
+    'A Hard Day\'s Night - Remastered 2009 - The Beatles',
+    'Can\'t Buy Me Love - Remastered 2009 - The Beatles',
+    'Love Me Do - Remastered 2009 - The Beatles',
+    'Penny Lane - Remastered 2009 - The Beatles',
+    'Yellow Submarine - Remastered 2009 - The Beatles'
+  ]
+  
+  const [currentSong, setCurrentSong] = useState('Something - Remastered 2009 - The Beatles');
+  const [queuedSongs, setQueuedSongs] = useState(songs);
+
 
   const handleNextButtonClick = () => {
     // Find the index of the current song
-    const currentIndex = songs.indexOf(currentSong);
+    const currentIndex = queuedSongs.indexOf(currentSong);
     
     // Move to the next song in the array
-    const nextIndex = (currentIndex + 1) % songs.length;
-    const nextSong = songs[nextIndex];
+    const nextIndex = (currentIndex + 1) % queuedSongs.length;
+    const nextSong = queuedSongs[nextIndex];
     
-    // Update the randomSong state with the next song
-    currentSong = nextSong;
+    // Update the currentSong state with the next song
+    setCurrentSong(nextSong);
 
     // Update the currently playing song
     console.log('Next button clicked');
-    console.log('Now playing:', currentSong);
-
-
+    console.log('Now playing:', nextSong);
   }
 
   const handlePreviousButtonClick = () => {
@@ -54,7 +68,7 @@ function MusicPlayer() {
             nextButtonClick={ handleNextButtonClick } 
             shuffleButtonClick={ handleShuffleButtonClick }
         />
-        <SongQueue />
+        <SongQueue queuedSongs={queuedSongs} setQueuedSongs={setQueuedSongs}/>
     </div>
   );
 }
--- src/components/MusicPlayer/musicPlayerComponents/SongQueue/SongQueue.jsx
+++ src/components/MusicPlayer/musicPlayerComponents/SongQueue/SongQueue.jsx
@@ -1,29 +1,19 @@
-import MusicPlayer from "../../MusicPlayer";
 
-export const songs = [
-  'Something - Remastered 2009 - The Beatles',
-  'Hey Jude - Remastered 2015 - The Beatles',
-  'Let It Be - Remastered 2009 - The Beatles',
-  'Help! - Remastered 2009 - The Beatles',
-  'Yesterday - Remastered 2009 - The Beatles',
-  'Come Together - Remastered 2009 - The Beatles',
-  'Twist And Shout - Remastered 2009 - The Beatles',
-  'I Want To Hold Your Hand - Remastered 2015 - The Beatles',
-  'A Hard Day\'s Night - Remastered 2009 - The Beatles',
-  'Can\'t Buy Me Love - Remastered 2009 - The Beatles',
-  'Love Me Do - Remastered 2009 - The Beatles',
-  'Penny Lane - Remastered 2009 - The Beatles',
-  'Yellow Submarine - Remastered 2009 - The Beatles'
-];
+function SongQueue({ queuedSongs, setQueuedSongs }) {
 
-function SongQueue({ queuedSongs }) {
+  const onRemove = (song) => {
+    const newQueue = queuedSongs.filter((queuedSong) => queuedSong !== song);
+    setQueuedSongs(newQueue);
+  }
+
   return (
     <div id='songQueueContainer'>
       <h2>Song Queue</h2>
       <ul id='songQueue'>
-        {songs.map((song) => (
+        {queuedSongs.map((song) => (
           <li key={song}>
-          <a href="" key={song}>{song}</a>
+            <a href="" key={song}>{song}</a>
+            <button onClick={() => { onRemove(song) }} ><i class="fa-solid fa-trash"></i></button>
           </li>
         ))}
       </ul>
