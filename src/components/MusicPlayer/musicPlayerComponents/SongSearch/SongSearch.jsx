02c8e589fe3966fe87335c346c6199da2bce0ccf
calebgjones <root@CALEB-POWER.localdomain>
2024-06-15 12:00:09 -0600

    More search changes

--- src/components/MusicPlayer/musicPlayerComponents/SongSearch/SongSearch.jsx
+++ src/components/MusicPlayer/musicPlayerComponents/SongSearch/SongSearch.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import setNowPlaying from '../MediaInterface/MediaInterface';
+import './SongSearch.css';
 
 // Search for any term within the songs data
 function SongSearch({ data, addSongToQueue }) {
   const [searchTerm, setSearchTerm] = React.useState('');
   const filteredSongs = data.filter((song) => {
-    return song.title.toLowerCase().includes(searchTerm.toLowerCase()) || song.artist.toLowerCase().includes(searchTerm.toLowerCase()) || song.album.toLowerCase().includes(searchTerm.toLowerCase()) || song.genre.toLowerCase().includes(searchTerm.toLowerCase());
+    const matchTitle = song.title.toLowerCase().includes(searchTerm.toLowerCase());
+    const matchArtist = song.artist.toLowerCase().includes(searchTerm.toLowerCase());
+    const matchAlbum = song.album.toLowerCase().includes(searchTerm.toLowerCase());
+    const matchGenre = song.genre.toLowerCase().includes(searchTerm.toLowerCase());
+
+    if (matchTitle || matchArtist || matchAlbum || matchGenre) {
+      return true;
+      
+    }
+
+    return false;
   });
 
-  const onAddSong = (song) => {
-    addSongToQueue(song);
-  }
-  
   const songList = filteredSongs.map((song, index) => (
-    <li key={index}>
-      <a style={{textDecoration: 'none'}} href='#' onClick={() => { setNowPlaying(song) }}>
-        {song.artist} - {song.title}
-      </a>
-      <button onClick={() => { onAddSong(song) }}>
-        <i className="fa-solid fa-plus"></i>
-      </button>
-    </li>
-  ));
+      <li id="songListItems" key={index} >
+        <a href='#' onClick={() => { setNowPlaying(song) }}>
+          {song.artist} - {song.title}
+        </a>
+        <button id="songListItems-addItem" onClick={() => { onAddSong(song) }}>
+          <i className="fa-solid fa-plus"></i>
+        </button>
+      </li>
+    ));
+  
+    const onAddSong = (song) => {
+      addSongToQueue(song);
+    }
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
@@ -29,8 +40,7 @@ function SongSearch({ data, addSongToQueue }) {
 
   return (
     <div id='SongSearchContainer'>
-      <input type='text' placeholder='Search for a song' value={searchTerm} onChange={handleSearchChange} />
-      <button>Search</button>
+      <input id='songSearch' type='text' placeholder='Search for a song / genre / album' value={searchTerm} onChange={handleSearchChange} />
       {songList}
     </div>
   );
