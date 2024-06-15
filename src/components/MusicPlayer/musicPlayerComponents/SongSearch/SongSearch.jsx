335fe68313ea636a7b44e799b7c038911126db4b
calebgjones <root@CALEB-POWER.localdomain>
2024-06-15 11:00:18 -0600

    Search update

--- src/components/MusicPlayer/musicPlayerComponents/SongSearch/SongSearch.jsx
+++ src/components/MusicPlayer/musicPlayerComponents/SongSearch/SongSearch.jsx
@@ -1,28 +1,34 @@
+import React from 'react';
+import setNowPlaying from '../MediaInterface/MediaInterface';
 
 function SongSearch({ data, addSongToQueue }) {
-
-  const songList = data.map((song) => {
-    return (
-      <li>
-        <a onClick={() => { onAddSong(song) }}>
-
-          {song.title}
-          <button onClick={() => { onAddSong(song) }}>
-            <i className="fa-solid fa-plus" ></i>
-          </button>
-
-        </a>
-      </li>
-    );
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const filteredSongs = data.filter((song) => {
+    return song.title.toLowerCase().includes(searchTerm.toLowerCase()) || song.artist.toLowerCase().includes(searchTerm.toLowerCase()) || song.album.toLowerCase().includes(searchTerm.toLowerCase()) || song.genre.toLowerCase().includes(searchTerm.toLowerCase());
   });
 
   const onAddSong = (song) => {
     addSongToQueue(song);
   }
+  
+  const songList = filteredSongs.map((song, index) => (
+    <li key={index}>
+      <a style={{textDecoration: 'none'}} href='#' onClick={() => { setNowPlaying(song) }}>
+        {song.artist} - {song.title}
+      </a>
+      <button onClick={() => { onAddSong(song) }}>
+        <i className="fa-solid fa-plus"></i>
+      </button>
+    </li>
+  ));
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
 
   return (
     <div id='SongSearchContainer'>
-      <input type='text' placeholder='Search for a song' />
+      <input type='text' placeholder='Search for a song' value={searchTerm} onChange={handleSearchChange} />
       <button>Search</button>
       {songList}
     </div>
