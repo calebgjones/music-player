e724ef25d79e0828fd6838c72f2db36fdf7adaea
calebgjones <root@CALEB-POWER.localdomain>
2024-07-24 22:22:41 -0600

    Progress bar update

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -11,6 +11,7 @@ function MusicPlayer({ createNotification }) {
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [queuedSongs, setQueuedSongs] = useState(songs);
   const [songHistory, setSongHistory] = useState([]);
+  const [getCurrentTime, setCurrentTime] = useState(0);
   const indexOfCurrentSong = queuedSongs.indexOf(currentSong);    // Find the index of the current song
 
   const [isPlaying, setIsPlaying] = useState(false);
@@ -26,11 +27,9 @@ function MusicPlayer({ createNotification }) {
 
   const addSongToQueue = (song) => {
     if (queuedSongs.includes(song)) {
-      createNotification(`Song already in queue: ${song.title}`, 'error');
       return;
     } else {
     setQueuedSongs([...queuedSongs, song]);
-    createNotification(`Song added to queue: ${song.title}`, 'success');
     }
   }
   
@@ -81,10 +80,12 @@ function MusicPlayer({ createNotification }) {
   const handlePlayPauseButtonClick = () => {
     const audioElement = document.getElementById('audioElement');
     if (audioElement.paused) {
-      audioElement.play();
+      audioElement.play().catch(error => {
+        console.log('Error playing audio:', error);
+      });
       console.log('Audio is playing');
       setIsPlaying(true);
-    } else {
+    } else if (!audioElement.ended && !audioElement.paused) {
       audioElement.pause();
       console.log('Audio is paused');
       setIsPlaying(false);
@@ -105,18 +106,18 @@ function MusicPlayer({ createNotification }) {
     shuffleSongs();
   }
 
-
-  const handleProgressBarChange = (event) => {
-    const currentTime = event.target.value;
-    const audioElement = document.getElementById('audioElement');
-    audioElement.currentTime = currentTime;
-  };
-
   const audioDuration = () => {
     const audioElement = document.getElementById('audioElement');
     const duration = audioElement.duration;
     return duration;
   }
+
+  const handleProgressBarChange = (event) => {
+    const currentTime = event.target.value;
+    const audioElement = document.getElementById('audioElement');
+    audioElement.currentTime = currentTime;
+    setCurrentTime(getCurrentTime);
+  };
 
   return (
     <>
@@ -132,13 +133,13 @@ function MusicPlayer({ createNotification }) {
 
             <input
               type="range"
+              value={getCurrentTime}
               min={0}
-              max={100}
-              value={currentSong.currentTime}
+              max={audioDuration}
               onChange={handleProgressBarChange}
             />
-            
-            <audio id="audioElement" controls src={'https://file-explorer-s3-bucket.s3.us-west-1.amazonaws.com/03.%20MERMAID.mp3?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjECgaCXVzLWVhc3QtMSJGMEQCIAo32kzLnK%2BkhKy2t32Nciyelw%2FrmzxIy4CdqTQmFdafAiB74AylSJI%2Bc49IhmdA%2FGfQ%2Fi9X9g48dRUHLdFA4kVXJSrxAgjB%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAQaDDM0OTY1NDA3ODM4OSIMNgV1ufMgAw7f7wePKsUCiZLs9aKyjuZL0BE6r0LtO26Qe7ESVOQwrn%2BKbKortK4BR9f5WP849gBRYtjM04yQdxjc0NNU1Vlmx9UMJraAdKInnJ3sHZCd7XB5PDw7TlFIsk6KY1cfhjGbdCcLKHzCmxq%2BguqexmkK9Aq6MOLf0HsVu4RM1JystIP6S%2BDrFjXqnl7yBa50MYITKukuKQ%2FKSm8tMMbWM5Xe%2Bb5G5QFn0%2FIjdXmTF4V3MGYQxFlwCV5XGKAXn%2FwGksv2DRAQgqWgd%2B8EGFSAlj6qeKeOXSUoA3TYxOp2jKFw3yH0edEGNGjtiyOTMTfEfLG0AH%2BluguzZSqwRMsKMNfHhC21OQm8smDWXFbLzYQCSiDvn8aqeVVnu48Ar985Ji1FWNj0ryt%2B8ROaE268CYdqINjNooat2UFMsufcLf8rkApx2NQatDF1Fi%2BqSTDZ57azBjq0AsGDt5AOmBC%2FXqUw4pErcshrnwv7IYt5JYt9Bir7BgC75s8%2BJ5TY%2FTfQ%2F5ZXnLvFXS39XfRKH04kPL9U1FmlIw4QaDK0FeBztg4nr7j46DGT1odbLZGKZmIk5Z3fqYR%2BbCweZzBcOYT0zzyEsGvgKaTfxtlPL73gtAdE%2BkpbaH4L%2F1FEdqEuVo3RrxCBYvGD1qf5n8dsnURO4g%2FKNkmLizy1v5t2aDMAXXiXsqMgcQuoo%2B1Q6GvQR%2F09XiZpOKfRV0PGcyO%2FoU%2ByIuen9gF%2F7ovbVDle7sG6txkF8YYI%2FQ75yp%2F%2B59z54oBRgAI7EeOy85%2BK3iTrGzEjEoSMzJPptzpOeKBC4wFxaLsuTcb21D404OLE2JBZ6vk8%2BbbA2a838z8kaoW8QP58czh7U7OrK0hJV6iD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240615T153307Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43200&X-Amz-Credential=ASIAVC2IB2O23UZ4NTZ7%2F20240615%2Fus-west-1%2Fs3%2Faws4_request&X-Amz-Signature=fb874c3f23471bf2b18da2d5b7f0021b3ee105a7e75875353e50e34427ff1313'} />
+
+            <audio id="audioElement" controls src={'https://music-player-bucket-test.s3.us-west-1.amazonaws.com/1f70779e-96c0-4473-aa58-a77b1ef7b920?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAVC2IB2O2QHLDIUGL%2F20240725%2Fus-west-1%2Fs3%2Faws4_request&X-Amz-Date=20240725T042051Z&X-Amz-Expires=3600&X-Amz-Signature=aaab999d5e448d9cc091105d27a0d1046daebc3d5e1ab7be75fb9bf1ff178156&X-Amz-SignedHeaders=host&x-id=GetObject'} />
             <MediaInterface
               previousButtonClick={handlePreviousButtonClick}
               playPauseButtonClick={handlePlayPauseButtonClick}
@@ -150,15 +151,12 @@ function MusicPlayer({ createNotification }) {
             <SongQueue queuedSongs={queuedSongs} setQueuedSongs={setQueuedSongs} setNowPlaying={setNowPlaying} />
           </div>
         </div>
+      </div>
+      <div id='searchBoxContainer'>
+        <div id='searchBox'>
+          <SongSearch data={songs} addSongToQueue={addSongToQueue}/>
         </div>
-        <div id='searchBoxContainer'>
-          <div id='searchBox'>
-            <SongSearch data={songs} addSongToQueue={addSongToQueue}/>
-          </div>
-        </div>
-
-
-
+      </div>
     </>
   );
 }
