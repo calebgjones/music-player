4be66981c8820a7174a67ac573ef39c4e9acc567
lichfiet <sortaminty@gmail.com>
2024-07-27 13:55:06 -0600

    removed default song queue, need to fix bug on delete song from queue

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -10,7 +10,7 @@ import songs from './songs.js';
 function MusicPlayer({ createNotification }) {
 
   const [currentSong, setCurrentSong] = useState(songs[0]);
-  const [queuedSongs, setQueuedSongs] = useState(songs);
+  const [queuedSongs, setQueuedSongs] = useState([]);
   const [songHistory, setSongHistory] = useState([]);
   const [getCurrentTime, setCurrentTime] = useState(0);
   const indexOfCurrentSong = queuedSongs.indexOf(currentSong);    // Find the index of the current song
@@ -28,13 +28,9 @@ function MusicPlayer({ createNotification }) {
 
 
   const addSongToQueue = (song) => {
-    if (queuedSongs.includes(song)) {
-      return;
-    } else {
     setQueuedSongs([...queuedSongs, song]);
-    }
-  }
-  
+  };
+
   const handleNextButtonClick = async () => {
     if (queuedSongs[1] === undefined || queuedSongs[1].title === 'No Song Playing') {
       setCurrentSong(emptySong);
@@ -44,9 +40,9 @@ function MusicPlayer({ createNotification }) {
     }
     const updatedQueue = queuedSongs.slice(1);    // Remove the first song from the queue
     setNowPlaying(updatedQueue[0]);   // Update the currentSong state with the next song
-  
+
     setQueuedSongs(updatedQueue); // Update the queuedSongs state with the updated queue
-  
+
     console.log('Song history After Next Button Click:', queuedSongs[1]);
   };
 
@@ -72,10 +68,10 @@ function MusicPlayer({ createNotification }) {
       console.log('wtf');
       return;
     } else {
-    setSongHistory([...songHistory, currentSong]);
-    queuedSongs.slice(indexOfCurrentSong, 0);
-    setCurrentSong(song);
-    createNotification(`Now playing: ${song.title}`, 'info');
+      setSongHistory([...songHistory, currentSong]);
+      queuedSongs.slice(indexOfCurrentSong, 0);
+      setCurrentSong(song);
+      createNotification(`Now playing: ${song.title}`, 'info');
     }
   };
 
@@ -150,14 +146,14 @@ function MusicPlayer({ createNotification }) {
               type="range"
               value={getCurrentTime}
               min={0}
-              //max={audioDuration}
+              max={audioDuration}
               onChange={handleProgressBarChange}
             />
 
             <audio id="audioElement" controls src={'https://music-player-bucket-test.s3.us-west-1.amazonaws.com/1f70779e-96c0-4473-aa58-a77b1ef7b920?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAVC2IB2O2QHLDIUGL%2F20240725%2Fus-west-1%2Fs3%2Faws4_request&X-Amz-Date=20240725T042051Z&X-Amz-Expires=3600&X-Amz-Signature=aaab999d5e448d9cc091105d27a0d1046daebc3d5e1ab7be75fb9bf1ff178156&X-Amz-SignedHeaders=host&x-id=GetObject'} />
             <MediaInterface
               previousButtonClick={handlePreviousButtonClick}
-              playPauseButtonClick={ handlePlayPauseButtonClick }
+              playPauseButtonClick={handlePlayPauseButtonClick}
               isPlaying={isPlaying}
               nextButtonClick={handleNextButtonClick}
               shuffleButtonClick={handleShuffleButtonClick}
@@ -171,7 +167,7 @@ function MusicPlayer({ createNotification }) {
       </div>
       <div id='searchBoxContainer'>
         <div id='searchBox'>
-          <SongSearch data={songs} addSongToQueue={addSongToQueue}/>
+          <SongSearch data={songs} addSongToQueue={addSongToQueue} />
         </div>
       </div>
       <div id='uploadBoxContainer'>
