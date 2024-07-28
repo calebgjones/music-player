b2106814fcc4fa049a27e3a8ba6b05a9ebbb4a45
lichfiet <sortaminty@gmail.com>
2024-07-27 18:10:10 -0600

    changed connectionurl for API to connect to ec2 instance

--- src/components/MusicPlayer/musicPlayerComponents/AlbumArt/AlbumArt.jsx
+++ src/components/MusicPlayer/musicPlayerComponents/AlbumArt/AlbumArt.jsx
@@ -1,8 +1,6 @@
 
 function AlbumArt() {
-  const randomWidth = Math.floor(Math.random() * (300 - 150 + 1) + 300);
-  const randomHeight = Math.floor(Math.random() * (300 - 150 + 1) + 300);
-  const randomAlbumArt = `https://via.placeholder.com/${randomWidth}x${randomHeight}`;
+  const randomAlbumArt = `https://via.placeholder.com/${300}x${300}`;
 
   return (
     <img className="albumArt" src={`${randomAlbumArt}`} alt="Album cover" />
